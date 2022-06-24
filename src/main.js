6c0f382c2e4413f1afc79d98578e325afc5c16d3
Your Name <petko.hupka@gmail.com>
2022-06-24 22:18:40 +0200

    build...

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { IonicVue } from "@ionic/vue";
-import "./registerServiceWorker";
 
 /* Ionic bullshit for css */
 import "@ionic/vue/css/core.css";
